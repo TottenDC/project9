793f6872faefb39afd3835c4ad61005feb1b7608
Daniel Totten <TottenDC@gmail.com>
2019-04-04 12:01:02 -0400

    Sets up for deployment on Github

---
+++ gallery_app/build/precache-manifest.9f87474752a0cc4fe1fea4fe94741284.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c147bd9d0b3c6dfb0fe721ec83cee742",
+    "url": "/project9/static/media/warning.c147bd9d.svg"
+  },
+  {
+    "revision": "e88df70f59597529ec0268a57b70f88c",
+    "url": "/project9/static/media/images.e88df70f.svg"
+  },
+  {
+    "revision": "5ce742e00ebccf7ccd23d75529e54901",
+    "url": "/project9/static/media/image.5ce742e0.svg"
+  },
+  {
+    "revision": "eb3cef12435f38c41242e373ce6c40a2",
+    "url": "/project9/static/media/camera.eb3cef12.svg"
+  },
+  {
+    "revision": "41f69f8166768c7c6885",
+    "url": "/project9/static/js/runtime~main.41f69f81.js"
+  },
+  {
+    "revision": "54637570b5f1084007a9",
+    "url": "/project9/static/js/main.54637570.chunk.js"
+  },
+  {
+    "revision": "65b14b4a3775d82f5214",
+    "url": "/project9/static/js/1.65b14b4a.chunk.js"
+  },
+  {
+    "revision": "54637570b5f1084007a9",
+    "url": "/project9/static/css/main.b301659d.chunk.css"
+  },
+  {
+    "revision": "35174c86e9200b05d7344a8ce940ce6b",
+    "url": "/project9/index.html"
+  }
+];
--- gallery_app/build/service-worker.js
+++ gallery_app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.865c9af8d7c3bc491e009bb702acbce1.js"
+  "/project9/precache-manifest.9f87474752a0cc4fe1fea4fe94741284.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/project9/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
