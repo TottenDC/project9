5ff13c1729191fb509bb263527a16019cc478a91
Daniel Totten <TottenDC@gmail.com>
2019-04-04 14:17:54 -0400

    Set up for deployment

---
+++ gallery_app/build/precache-manifest.0d9589ab230e0f72bb274bd43d6e9cee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c147bd9d0b3c6dfb0fe721ec83cee742",
+    "url": "/static/media/warning.c147bd9d.svg"
+  },
+  {
+    "revision": "e88df70f59597529ec0268a57b70f88c",
+    "url": "/static/media/images.e88df70f.svg"
+  },
+  {
+    "revision": "5ce742e00ebccf7ccd23d75529e54901",
+    "url": "/static/media/image.5ce742e0.svg"
+  },
+  {
+    "revision": "eb3cef12435f38c41242e373ce6c40a2",
+    "url": "/static/media/camera.eb3cef12.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2eddd2ee189d2ccb910d",
+    "url": "/static/js/main.2eddd2ee.chunk.js"
+  },
+  {
+    "revision": "65b14b4a3775d82f5214",
+    "url": "/static/js/1.65b14b4a.chunk.js"
+  },
+  {
+    "revision": "2eddd2ee189d2ccb910d",
+    "url": "/static/css/main.b301659d.chunk.css"
+  },
+  {
+    "revision": "772b7829b92b55b4e62c411a80214db7",
+    "url": "/index.html"
+  }
+];
